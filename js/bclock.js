99a32c93b11a8767f5c11f5f0b7a0bdea5e1d341
kumanofoo <kumanofoo@gmail.com>
2024-12-04 17:56:56 +0900

    feat: air quality

--- js/bclock.js
+++ js/bclock.js
@@ -70,6 +70,70 @@ if ('wakeLock' in navigator) {
   })
 }
 
+const toPercent = (minimum, maximum, value) => {
+  let v = Math.max(minimum, value);
+  v = Math.min(maximum, value);
+  return parseInt((v - minimum)*100/(maximum - minimum));
+}
+
+const temperature = document.querySelector("#air-temperature");
+temperature.graph = document.querySelector("#air-temperature-graph");
+temperature.minimum = 15;
+temperature.maximum = 35;
+temperature.unitName = "℃";
+temperature.jsonKey = "temperature_deg_c";
+temperature.container = document.querySelector("#temperature-container");
+const humidity = document.querySelector("#air-humidity");
+humidity.graph = document.querySelector("#air-humidity-graph");
+humidity.minimum = 0;
+humidity.maximum = 100;
+humidity.unitName = "%";
+humidity.jsonKey = "humidity_percent";
+humidity.container = document.querySelector("#humidity-container");
+const co2 = document.querySelector("#air-co2");
+co2.graph = document.querySelector("#air-co2-graph");
+co2.minimum = 300;
+co2.maximum = 1300;
+co2.unitName = "ppm";
+co2.jsonKey = "co2_ppm";
+co2.container = document.querySelector("#co2-container");
+const airQualityItem = [temperature, humidity, co2];
+const airQuality = async () => {
+  const url = window.location.origin + "/air_quality.json";
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const json = await response.json();
+      console.log(json);
+      airQualityItem.forEach((element) => {
+        let v = element.jsonKey in json ? json[element.jsonKey] : null;
+        if (v !== null) {
+          element.container.style.display = "";
+            const p = toPercent(element.minimum, element.maximum, v);
+          const graph = (element.graph.innerText + String.fromCharCode(0x100 + p)).slice(-60);
+          element.innerText = v + element.unitName;
+          element.graph.innerText = graph;  
+        }
+        else {
+          temperature.innerText = "";
+          temperature.graph.innerText = "";  
+          element.container.style.display = "none";
+        }  
+      });
+    }
+    else {
+      airQualityItem.forEach((element) => {
+        element.innerText = "";
+        element.graph.innerText = "";
+        element.container.style.display = "none";
+      })
+    }
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
 window.onload = () => {
   let displayArea = document.querySelector("#display-area");
   displayArea.addEventListener('dblclick', () => {
@@ -84,4 +148,6 @@ window.onload = () => {
 
   bclock();
   setInterval(bclock, 1000);
+  airQuality();
+  setInterval(airQuality, 60000);
 }
