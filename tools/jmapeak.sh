6cfdf314b65c10686d9a1f554a0aa0906b3f8aeb
kumanofoo <kumanofoo@gmail.com>
2025-04-14 12:58:47 +0900

    fix: adjust daily temperature output timing
    Update temperature output logic based on time of day
    
    - Max temp uses next day's value from 17:00 onwards.
    - Min temp uses next day's value from 05:00 onwards.

--- tools/jmapeak.sh
+++ tools/jmapeak.sh
@@ -5,12 +5,12 @@ verbose=false
 while getopts "v" opt; do
     case ${opt} in
 	v ) verbose=true ;;
-	* ) printf "Usage: $0 [-v] <office_code> <AMeDAS_site_code>\n"; exit 1 ;;
+	* ) printf "Usage: $0 [-v] <office_code> <AMeDAS_site_code>\n" 1>&2; exit 1 ;;
     esac
 done
 shift $((OPTIND -1))
 if [ "$#" -ne 2 ]; then
-    printf "usage: $0 [-v] <office_code> <AMeDAS_site_code>\n"
+    printf "Usage: $0 [-v] <office_code> <AMeDAS_site_code>\n" 1>&2
     exit 1
 fi
 
@@ -27,27 +27,45 @@ area_temps=$(jq -r '.temps[]' <<< $area)
 
 time_define_arr=($time_define)
 area_temps_arr=($area_temps)
-current_ts=$(date +%s)
-lowest=""
-highest=""
-for i in "${!time_define_arr[@]}"; do
-    temp_ts=$(date -d "${time_define_arr[i]}" +%s)
-    if [ "$current_ts" -lt "$temp_ts" ]; then
-	if [ "$(date -d "${time_define_arr[i]}" +%H)" -lt 9 ]; then
-	    lowest=${area_temps_arr[i]}
-	    lowest_dt=${time_define_arr[i]}
-	else
-	    highest=${area_temps_arr[i]}
-	    highest_dt=${time_define_arr[i]}
-	fi
+if [ "${#time_define_arr[@]}" -ne "${#area_temps_arr[@]}" ]; then
+    printf "The sizes of the 'timeDefines' and 'temps' arrays do not match.\n" 1>&2
+    exit 1
+fi
+
+current_hour=$(date +%H)
+if [ "$current_hour" -lt 5 ]; then
+    # night
+    if [ "${#time_define_arr[@]}" -lt 2 ]; then
+	printf "There is not enough night data.\n" 1>&2
+	exit 1
     fi
-    if [ -n "$lowest" ] && [ -n "$highest" ]; then
-	break
+    low_i=0
+    high_i=1
+elif [ "$current_hour" -lt 17 ]; then
+    # day
+    if [ "${#time_define_arr[@]}" -lt 3 ]; then
+	printf "There is not enough daytime data.\n" 1>&2
+	exit 1
     fi
-done
+    low_i=2
+    high_i=0
+else
+    # night
+    if [ "${#time_define_arr[@]}" -lt 2 ]; then
+	printf "There is not enough evening data.\n" 1>&2
+	exit 1
+    fi
+    low_i=0
+    high_i=1
+fi
+lowest=${area_temps_arr[$low_i]}
+lowest_dt=${time_define_arr[low_i]}
+highest=${area_temps_arr[$high_i]}
+highest_dt=${time_define_arr[high_i]}
 
 if [ "$verbose" = true ]; then
-    printf "{ \"area_name\": \"$area_name\", "
+    printf "{ \"current_dt\": \"$(date --iso-8601=seconds)\", "
+    printf "\"area_name\": \"$area_name\", "
     printf "\"lowest_dt\": \"$lowest_dt\", "
     printf "\"lowest\": $lowest, "
     printf "\"highest_dt\": \"$highest_dt\", "
